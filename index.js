91c68fd61e81c84b0118b2afa61c40b79c6f9006
raul <raulehernandeza@gmail.com>
2021-03-17 19:40:25 -0400

    segunda subida del servidor

--- index.js
+++ index.js
@@ -9,13 +9,14 @@ app.use(express.json())
 app.use(require('./server/routes/index'));
 
 require('./server/database/db');
-process.env.PORT = process.env.PORT || 3000;
+process.env.PORT = process.env.PORT || 'dev';
 
 app.get('/',function(req,res){
 console.log("hola")
 res.send(200);
 
 })
+
 process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 app.listen(process.env.PORT, ()=> {
     console.log("Escuchando en puerto tal o 3000");
