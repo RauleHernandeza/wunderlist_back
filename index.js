ec09df21861da0a2a7f7b51e5d53fe24b84ff0aa
raul <raulehernandeza@gmail.com>
2021-03-17 20:13:06 -0400

    primera subida del servidor

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const express = require('express')
 const app = express();
-const dd = http.createserver(app);
 const path = require('path');
 const cors=require('cors');
 
@@ -10,16 +9,15 @@ app.use(express.json())
 app.use(require('./server/routes/index'));
 
 require('./server/database/db');
-app.set('port', process.env.PORT || 3000);
+process.env.PORT = process.env.PORT || 3000;
 
 app.get('/',function(req,res){
 console.log("hola")
 res.send(200);
 
 })
-
 process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
-dd.listen(app.get('port'), () => {
-    console.log("Escuchando en puerto tal o 3000");
+app.listen(process.env.PORT, ()=> {
+    console.log("Escuchando en puerto 3000");
 })
 
