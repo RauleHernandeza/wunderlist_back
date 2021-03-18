e9502973a64e78a34f1f51f962b2e3ff6f8a90ff
raul <raulehernandeza@gmail.com>
2021-03-17 20:08:52 -0400

    primer intento

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const app = express();
+const dd = http.createserver(app);
 const path = require('path');
 const cors=require('cors');
 
@@ -9,15 +10,16 @@ app.use(express.json())
 app.use(require('./server/routes/index'));
 
 require('./server/database/db');
-process.env.PORT = process.env.PORT || 3000;
+app.set('port', process.env.PORT || 3000);
 
 app.get('/',function(req,res){
 console.log("hola")
 res.send(200);
 
 })
+
 process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
-app.listen(process.env.PORT, ()=> {
-    console.log("Escuchando en puerto 3000");
+dd.listen(app.get('port'), () => {
+    console.log("Escuchando en puerto tal o 3000");
 })
 
